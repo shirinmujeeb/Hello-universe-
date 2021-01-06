acea0d69eb9864f66a6d19a60052abf6be260e07
Shirin Mujeeb <76579730+shirinmujeeb@users.noreply.github.com>
2021-01-06 16:43:15 +1000

    Add files via upload

---
+++ hello universe1/customview.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+struct customview:View {
+    var body:some View{
+        
+            Text("Hello Universe")
+            .frame(width: 5000, height: 5000)
+                .background(Image("HelloUniverse_Background"))
+        
+    }
+}
+
---
+++ hello universe1/hello_universe1App.swift
@@ -0,0 +1,17 @@
+//
+//  hello_universe1App.swift
+//  hello universe1
+//
+//  Created by shir on 6/1/21.
+//
+
+import SwiftUI
+
+@main
+struct hello_universe1App: App {
+    var body: some Scene {
+        WindowGroup {
+           customview()
+        }
+    }
+}
